de7bcbe2dca7a0c9b95bcd75918c29b4b2dcd3a8
klequis <klequis@gmail.com>
2019-04-12 19:52:35 +0000

    initial

---
+++ hello.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('also try /test')
+}
+app.get('/test, (req, res) => {
+  res.send('test works')
+}
+
+app.listen(port, () => {
+  console.log(`Events API server is listening on port ${port}`)
+})
