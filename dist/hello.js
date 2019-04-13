d76e33cea1dbb42dd58092f6ffb897ea9b0bc629
klequis <klequis@gmail.com>
2019-04-13 07:27:30 -0700

    start-local script, @babel/node & mongo connection test

--- dist/hello.js
+++ dist/hello.js
@@ -4,10 +4,10 @@ var _express = _interopRequireDefault(require("express"));
 
 var _bodyParser = _interopRequireDefault(require("body-parser"));
 
+var _mongodb = _interopRequireDefault(require("mongodb"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-// const express = require('express')
-// const bodyParser = require('body-parser')
 var app = (0, _express["default"])();
 var port = 3000;
 app.use(_bodyParser["default"].json());
@@ -22,4 +22,28 @@ app.get('/test', function (req, res) {
 });
 app.listen(port, function () {
   console.log("Events API server is listening on port ".concat(port));
-});
+}); // MongoDB test
+// const MongoClient = mongodb.MongoClient
+// const mongoUrl = 'mongodb+srv://todo-db-admin:D92dARWONO0t16uF@todo-cluster0-ilc7v.mongodb.net/test?retryWrites=true'
+// const dbName = 'todo-test'
+// let client
+// async function connectDB() {
+//   if (!client) {
+//     client = await MongoClient.connect(mongoUrl)
+//   }
+//   return {
+//       db: client.db(dbName),
+//       client: client
+//   }
+// }
+// async function testConnection() {
+//   try {
+//     const { db, client } = await connectDB()
+//     const ret = await db.collection('todos').find({}).toArray()
+//     console.log('SUCCESS', ret)
+//   }
+//   catch (e) {
+//     console.log('ERROR: dbFunctions.find', e.message)
+//   }
+// }
+// testConnection()
