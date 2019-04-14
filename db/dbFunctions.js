7948d8e0824c446a5a6091ec89b2e8a1166e487c
klequis <klequis@gmail.com>
2019-04-14 07:34:45 -0700

    move mongo connect test to /db

--- db/dbFunctions.js
+++ db/dbFunctions.js
@@ -133,3 +133,36 @@ export const insertMany = async (collection, data) => {
     return returnError(e)
   }
 }
+
+
+// MongoDB test
+/*
+const MongoClient = mongodb.MongoClient
+
+const mongoUrl = 'mongodb+srv://todo-db-admin:D92dARWONO0t16uF@todo-cluster0-ilc7v.mongodb.net/test?retryWrites=true'
+const dbName = 'todo-dev'
+let client
+
+async function connectDB() {
+  if (!client) {
+    client = await MongoClient.connect(mongoUrl)
+  }
+  return {
+      db: client.db(dbName),
+      client: client
+  }
+}
+
+async function testConnection() {
+  try {
+    const { db, client } = await connectDB()
+    const ret = await db.collection('todos').find({}).toArray()
+    console.log('SUCCESS', ret)
+  }
+  catch (e) {
+    console.log('ERROR: dbFunctions.find', e.message)
+  }
+}
+
+testConnection()
+*/
--- server/index.js
+++ server/index.js
@@ -19,34 +19,3 @@ app.get('/test', (req, res) => {
 app.listen(port, () => {
   console.log(`Events API server is listening on port ${port}`)
 })
-
-// MongoDB test
-
-const MongoClient = mongodb.MongoClient
-
-const mongoUrl = 'mongodb+srv://todo-db-admin:D92dARWONO0t16uF@todo-cluster0-ilc7v.mongodb.net/test?retryWrites=true'
-const dbName = 'todo-dev'
-let client
-
-async function connectDB() {
-  if (!client) {
-    client = await MongoClient.connect(mongoUrl)
-  }
-  return {
-      db: client.db(dbName),
-      client: client
-  }
-}
-
-async function testConnection() {
-  try {
-    const { db, client } = await connectDB()
-    const ret = await db.collection('todos').find({}).toArray()
-    console.log('SUCCESS', ret)
-  }
-  catch (e) {
-    console.log('ERROR: dbFunctions.find', e.message)
-  }
-}
-
-testConnection()
